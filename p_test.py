a2bb4eaad7498abe21fdd0e607d5086147c64fee
wanghui <353712545@qq.com>
2019-11-22 23:21:16 +0800

    python测试

--- p_test.py
+++ p_test.py
@@ -1,2 +1,21 @@
-while True:
-    print("\a\a\a\a\a\a\a\a\a\a\a")
+class vec:
+
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __sig(self):
+        print("aaaa")
+
+    def __str__(self):
+        return 'vec(%d, %d)' % (self.a, self.b)
+
+    def __add__(self, other):
+        return vec(self.a + other.b, self.b + other.a)
+
+
+a = vec(1, 10)
+b = vec(2, 8)
+c = a+b
+print()
