f800dfbe9b07392a8b6e4a960cedb3f8d390d570
wanghui <353712545@qq.com>
2019-12-04 00:42:58 +0800

    python测试

--- p_test.py
+++ p_test.py
@@ -1,21 +1,7 @@
-class vec:
 
 
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
 
-    def __sig(self):
-        print("aaaa")
-
-    def __str__(self):
-        return 'vec(%d, %d)' % (self.a, self.b)
-
-    def __add__(self, other):
-        return vec(self.a + other.b, self.b + other.a)
-
-
-a = vec(1, 10)
-b = vec(2, 8)
-c = a+b
-print()
+a = [1 ,2, 3, 4, 5]
+a = tuple(a)
+print(a)
+print(*a)
