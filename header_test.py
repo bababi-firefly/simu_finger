a925488c200de45b9eec19a182f8e961ee07556b
BABABI <353712545@qq.com>
2019-12-13 17:04:36 +0800

    uA

--- header_test.py
+++ header_test.py
@@ -4,16 +4,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from time import sleep
 import numpy as np
 
-browser = webdriver.Chrome()
-browser.get("https://whatismyipaddress.com/blacklist-check")
-button = browser.find_element_by_name("Lookup Hostname")
-button.click()
-all_img = browser.find_elements_by_xpath("//table//img")
-for i in all_img:
-    print(i.get_attribute('src'))
+# 识别selenium的方法
+# 1 window.navigator.webdriver的值为True
+#    应对方法 options.add_experimental_option('excludeSwitches', ['enable-automation'])
+# 2 通过查找js中是否包含$cdc_类的变量来确定
+#    应对方法，vim编辑chromedriver文件，并将此串替换为等长的其它串
+# 3 window.navigator.languages中不要出现zh中文标识等
+#   window.navigator.language应为en
+
+driver1 = r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver"
+driver2 = r"C:\Users\BABABI\Desktop\chromedriver"
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-automation'])
+browser = webdriver.Chrome(executable_path=driver2)
+browser.get("http://www.baidu.com")
 
 
 
--- test.py
+++ test.py
@@ -1,4 +1,1 @@
-from useful_fuc import *
-from fake_useragent import UserAgent
-ua = UserAgent()
-print(ua.ie)
+
